f0f63b67efd4f7f980ab165638a751723cbf3c82
pointmina <pointmina@gmail.com>
2023-05-20 23:43:03 +0900

    web13

---
+++ timer.js
@@ -0,0 +1,22 @@
+let count = 0;
+let timerID = null;
+
+onmessage = function(e){
+    if(e.data == "start"){
+        if(timerID != null)
+            return;
+        timerID = setInterval(myCallback, 1000);
+    }
+    
+    else if(e.data == "stop"){
+        if(timerID == null)
+            return;
+        clearInterval(timerID);
+        close();
+    }
+}
+
+function myCallback(){
+    count++;
+    postMessage(count);
+}
